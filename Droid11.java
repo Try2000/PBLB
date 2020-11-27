9526ec3b7ba79c3a3ee2bf46fa13db34ef299859
Nakata <55831247+Try2000@users.noreply.github.com>
2020-11-27 15:45:09 +0900

    Add files via upload

---
+++ Droid11.java
@@ -0,0 +1,30 @@
+package testTeam;
+
+import robocode.*;
+import java.io.IOException;
+
+public class Droid11 extends TeamRobot implements Droid{
+	public void run() {
+		out.println("MyFirstDroid ready.");
+		while (true) {
+			ahead(100);//test
+			back(100);//test
+		}
+	}
+
+	public void onMessageReceived(MessageEvent e) {
+		if (e.getMessage() instanceof RobotColors) {
+			RobotColors c = (RobotColors) e.getMessage();
+			
+			setBodyColor(c.bodyColor);
+			setGunColor(c.gunColor);
+			setRadarColor(c.radarColor);//don't have
+			setScanColor(c.scanColor);//don't have
+			setBulletColor(c.bulletColor);
+		}
+	}
+	
+	public void onDeath(DeathEvent e) {
+		out.println("I died");//test
+	}
+}
