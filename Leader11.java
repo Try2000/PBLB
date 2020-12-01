bc0eae997e6bcc429470fc4b74231e65d38cb174
nonomura56 <72593334+nonomura56@users.noreply.github.com>
2020-12-01 23:41:56 +0900

    Update Leader11.java

--- Leader11.java
+++ Leader11.java
@@ -9,6 +9,7 @@ import java.util.LinkedList;
 import java.io.IOException;
 
 public class Leader11 extends TeamRobot {
+	LinkedList<String> enemyName = new LinkedList<String>();
 	LinkedList<EnemyInfo> enemyInfo = new LinkedList<EnemyInfo>();
 	
 	public void run() {
@@ -27,6 +28,7 @@ public class Leader11 extends TeamRobot {
 		
 		int frame = 0;
 		while(true) {
+			//swing head for search
 			if((0 <= frame && frame <= 2) || (9 <= frame && frame <= 11)) {
 				this.setTurnRadarRight(45);
 				this.setTurnGunRight(20);
@@ -43,13 +45,32 @@ public class Leader11 extends TeamRobot {
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
-		if(e.getName().equals("testTeam.Droid11* (2)") || e.getName().equals("testTeam.Droid11* (3)")) {//not good
+		if(e.getName().equals("testTeam.Droid11* (2)") 
+				|| e.getName().equals("testTeam.Droid11* (3)")) {//case:teammate //not good code
+			
 			setAhead(100);//test
 			out.println("teammate");//test
+			
 			return;
-		}else {
+		}else {//case:enemy
 			setTurnLeft(60);//test
 			out.println("enemy");//test
+			
+			int index = enemyName.indexOf(e.getName());
+			if(-1 == index) {//if list doesn't have data of that robot
+				enemyName.add(e.getName());//add to list
+				enemyInfo.add(new EnemyInfo(e, this));//add to list
+				
+				out.println(enemyName.get(enemyName.indexOf(e.getName())));//test
+			}else {
+				enemyInfo.get(index).update(e, this);
+				
+				//broadcast to Droid
+				
+				out.println("name:" + enemyName.get(index) + " x:" + enemyInfo.get(index).getX()
+						+ ", y:" + enemyInfo.get(index).getY());//test
+			}
+			
 		}
 	}
 }
