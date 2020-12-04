c4cc67f2d97ee3e35de2b6d7d335da76c624af6a
kohuku <daikiti8090@gmail.com>
2020-12-04 17:16:35 +0900

    add 'speed' and 'direction' as new factor.

--- EnemyInfo.java
+++ EnemyInfo.java
@@ -4,12 +4,19 @@ import robocode.*;
 
 public class EnemyInfo implements java.io.Serializable {
 	private static final long serialVersionUID = 7203604189743390550L;
-		
-	private double prex;
-	private double prey;
-	private double nowx;
-	private double nowy;
-	
+
+	private double x;
+	private double y;
+	private double speed;
+	private double direction;
+
 	private double dx;
 	private double dy;
+
+	public EnemyInfo(double nx,double ny,double nspeed,double ndirection) {
+		x=nx;
+		y=ny;
+		speed=nspeed;
+		direction=ndirection;
+	}
 }
