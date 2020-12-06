2eb6e65a66591db61b2e993eaaa8c4b857be0deb
nonomura56 <72593334+nonomura56@users.noreply.github.com>
2020-12-06 21:20:35 +0900

    Add files via upload

---
+++ LeaderInfo.java
@@ -0,0 +1,35 @@
+package group11;
+
+import java.io.Serializable;
+import robocode.*;
+
+public class LeaderInfo implements Serializable {
+	private static final long serialVersionUID = 5163422492696388736L;
+	
+	public LeaderInfo(TeamRobot leader11) {
+		set(leader11);
+	}
+	
+	private double x;
+	private double y;
+	
+	/*nakata*/
+	public boolean isalive = true;
+	/**/
+	
+	private void set(TeamRobot leader11) {
+		x = leader11.getX();
+		y = leader11.getY();
+	}
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	
+	public void update(TeamRobot leader11) {
+		set(leader11);
+	}
+}
