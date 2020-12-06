af225a309d54a3929b35f56dd14a9840ef86c758
nonomura56 <72593334+nonomura56@users.noreply.github.com>
2020-12-06 10:59:12 +0900

    Update Leader11.java

--- Leader11.java
+++ Leader11.java
@@ -1,20 +1,102 @@
-package testTeam;
+package group11;
 
 import robocode.*;
 
-import java.util.Arrays;
 import java.util.LinkedList;
-
+import java.awt.geom.Point2D;
 //import java.awt.Color;
 import java.io.IOException;
 
-public class Leader11 extends TeamRobot {
+public class G11_Leader extends TeamRobot {
 	//informations
 	LinkedList<String> enemyName = new LinkedList<String>();
 	LinkedList<EnemyInfo> enemyList = new LinkedList<EnemyInfo>();
 	
+	int frame = 0;//for swinging head
+	int teammates = 2;//the number of living teammates
+	
+	/*nonomura test*/
+	Point2D.Double p;
+	/**/
+	
 	public void run() {
-		//set color
+		setColors();
+		
+		
+		//information of leader
+		LeaderInfo leaderInfo = new LeaderInfo(this);
+		
+		while(true) {
+			doScanner();
+			
+			//broadcast information of leader
+			leaderInfo.update(this);
+			try {
+				broadcastMessage(leaderInfo);
+			}catch(IOException ignored) {
+				out.println("LeaderInfo class was ignored");
+			}
+			
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if(e.getName().equals("group11.G11_Sub1*") 
+				|| e.getName().equals("group11.G11_Sub2*")) {//case:teammate
+	
+			setAhead(100);//test
+			
+			return;
+		}else {//case:enemy
+			setTurnLeft(60);//test
+			
+			int index = enemyName.indexOf(e.getName());
+			if(-1 == index) {//if list doesn't have data of that robot
+				enemyName.add(e.getName());//add to list
+				enemyList.add(new EnemyInfo(e, this));//add to list
+				
+			}else {
+				enemyList.get(index).update(e, this);
+				
+				/*prediction test*/
+				p = enemyList.get(index).prediction(this, 1);
+				out.println("nextX:" + p.getX() + ", nextY:" + p.getY());
+				/**/
+			}
+			
+			/*prediction test*/
+			if(e.getName().equals(enemyName.get(0))){//found [0] robot
+				try {
+					broadcastMessage(enemyList.get(0));
+				}catch(IOException g) {
+					out.println("enemyinfo was ignored");
+				}
+			}
+			/**/
+			
+		}
+	}
+	
+	public void onRobotDeath(RobotDeathEvent e) {
+		if(e.getName().equals("group11.G11_Sub1*") 
+				|| e.getName().equals("group11.G11_Sub2*")) {//case:teammate
+			teammates--;
+			
+			out.println("teammate died");//test
+		}else {//case:enemy
+			int index = enemyName.indexOf(e.getName());
+			if(-1 == index) {//leader hasn't found the robot yet
+				//do no action
+			}else {//remove the data of dead enemy
+				
+				enemyName.remove(index);
+				enemyList.remove(index);
+				
+			}
+		}
+	}
+	
+	public void setColors() {
 		RobotColors c = new RobotColors();
 		setBodyColor(c.bodyColor);
 		setGunColor(c.gunColor);
@@ -26,68 +108,19 @@ public class Leader11 extends TeamRobot {
 		}catch(IOException ignored) {
 			out.println("RobotColors class was ignored");
 		}
-		
-		//information of leader
-		LeaderInfo leaderInfo = new LeaderInfo(this);
-		
-		//for swinging head
-		int frame = 0;
-		while(true) {
-			//swing head for search
-			if((0 <= frame && frame <= 2) || (9 <= frame && frame <= 11)) {
-				this.setTurnRadarRight(45);
-				this.setTurnGunRight(20);
-				this.execute();
-			}else if(3 <= frame && frame <= 8) {
-				this.setTurnRadarLeft(45);
-				this.setTurnGunLeft(20);
-				this.execute();
-			}
-			frame = (frame + 1) % 12;
-			
-			//broadcast information of leader
-			leaderInfo.update(this);
-			try {
-				broadcastMessage(leaderInfo);
-			}catch(IOException ignored) {
-				out.println("LeaderInfo class was ignored");
-			}
-			
-			out.println("LeaderX:" + this.getX() + ", LeaderY:" + this.getY());//test
-			out.println(Arrays.toString(this.getTeammates()));//test
-		}
 	}
 	
-	public void onScannedRobot(ScannedRobotEvent e) {
-		if(e.getName().equals("testTeam.Droid11* (2)") 
-				|| e.getName().equals("testTeam.Droid11* (3)")) {//case:teammate //not good code
-			
-			setAhead(100);//test
-			out.println("teammate");//test
-			
-			return;
-		}else {//case:enemy
-			setTurnLeft(60);//test
-			out.println("enemy");//test
-			
-			int index = enemyName.indexOf(e.getName());
-			if(-1 == index) {//if list doesn't have data of that robot
-				enemyName.add(e.getName());//add to list
-				enemyList.add(new EnemyInfo(e, this));//add to list
-				
-				out.println(enemyName.get(enemyName.indexOf(e.getName())));//test
-			}else {
-				enemyList.get(index).update(e, this);
-				
-				//broadcast to Droid
-				
-				out.println("name:" + enemyName.get(index) + " x:" + enemyList.get(index).getX()
-						+ ", y:" + enemyList.get(index).getY());//test
-			}
-			
+	public void doScanner() {
+		//swing head for search
+		if((0 <= frame && frame <= 2) || (9 <= frame && frame <= 11)) {
+			this.setTurnRadarRight(45);
+			this.setTurnGunRight(20);
+			this.execute();
+		}else if(3 <= frame && frame <= 8) {
+			this.setTurnRadarLeft(45);
+			this.setTurnGunLeft(20);
+			this.execute();
 		}
+		frame = (frame + 1) % 12;
 	}
-	
-	//remove the data of dead enemy
-	
 }
